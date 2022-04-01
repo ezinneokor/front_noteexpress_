2bed1f6a07b6a71da31eedb8767ce256b8f6c88a
EzinneOkor <ezinnejohnndukwe5@gmail.com>
2022-04-01 05:04:37 +0100

    Added Toast

--- custom/script.js
+++ custom/script.js
@@ -21,7 +21,45 @@ createPostForm.addEventListener("submit", function(e){
         }
         axios.post("http://localhost:4000/create-post", params).then((feedback) =>{
             //continue from here..
-            console.log(feedback);
+            // console.log(feedback);
+
+             if(feedback.data.code){
+                console.log(feedback);
+              
+
+                let toastCode = `
+                <div class="position-fixed bottom-0 right-0 p-3" style="z-index: 5; left: 0; top: 0;">
+                <div id="toast-id" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-delay="2000">
+                  <div class="toast-header bg-success text-white">  
+                    <strong class="mr-auto">Success</strong>
+                    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="toast-body">
+                   ${feedback.data.message}
+                  </div>
+                </div>
+              </div>
+  ${feedback.data.message}
+  
+  </div>
+</div>
+                `
+
+                let toastElementDiv = document.createElement("div");
+
+              toastElementDiv.innerHTML = toastCode;
+
+             document.body.appendChild(toastElementDiv);
+
+             $('#toast-id').toast('show')
+
+             $('#create-post-modal').modal('hide');
+
+
+
+             }
 
         })
 
